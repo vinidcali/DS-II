b3c7ed2985465ffdf0fad111b5a3d54825990845
Vini <vinidcali@gmail.com>
2014-05-07 16:01:58 -0300

    PreV1 5

--- Iteração I/Elaboração/sql.sql
+++ Iteração I/Elaboração/sql.sql
@@ -31,7 +31,7 @@ CREATE TABLE Etapa (
 	id INTEGER NOT NULL AUTO_INCREMENT,
 	codigo VARCHAR(20) NOT NULL,
 	disc_id INTEGER NOT NULL,
-	PRIMARY KEY (id, disc_id),
+	PRIMARY KEY (id),
 	UNIQUE KEY (codigo),
 	FOREIGN KEY (disc_id) REFERENCES Disciplina (id)
 ) ENGINE=InnoDB;
@@ -40,7 +40,7 @@ CREATE TABLE Calendario (
 	id INTEGER NOT NULL AUTO_INCREMENT,
 	disc_id INTEGER NOT NULL,
 	nome VARCHAR (20) NOT NULL,
-	PRIMARY KEY (id, disc_id),
+	PRIMARY KEY (id),
 	UNIQUE KEY (nome),
 	FOREIGN KEY (disc_id) REFERENCES Disciplina (id)
 ) ENGINE=InnoDB;
@@ -50,8 +50,36 @@ CREATE TABLE Aula (
 	conteudo VARCHAR(1000),
 	dataHora DATETIME NOT NULL,
 	cal_id INTEGER NOT NULL,
-	cal_disc_id INTEGER NOT NULL,
-	PRIMARY KEY (id, cal_id, cal_disc_id),
+	PRIMARY KEY (id),
 	UNIQUE KEY (dataHora),
-	FOREIGN KEY (cal_id, cal_disc_id) REFERENCES Calendario (id, disc_id)
+	FOREIGN KEY (cal_id) REFERENCES Calendario (id)
 ) ENGINE=InnoDB;
+
+
+INSERT INTO Pessoa VALUES
+	(NULL, "Pessoa 1", "p1@gmail.com", "p1", 00001),
+    (NULL, "Pessoa 2", "p2@gmail.com", "p2", 00002),
+    (NULL, "Pessoa 3", "p2@gmail.com", "p3", 00003)
+;
+
+INSERT INTO Professor (id) VALUES (1), (2);
+
+INSERT INTO Disciplina VALUES
+	(NULL, "Banco de Dados I", NULL, "ES.BD1", 1),
+	(NULL, "Programação I", 60, "EM.P1", 2)
+;
+
+INSERT INTO Calendario VALUES
+	(NULL, 1, "ADS2012"),
+	(NULL, 2, "TEC2014")
+;
+
+INSERT INTO Aula VALUES 
+	(NULL, NULL, "2014-05-05 19:00:00", 1),
+	(NULL, NULL, "2014-05-10 19:00:00", 1),
+	(NULL, NULL, "2014-05-13 19:00:00", 1),
+	(NULL, NULL, "2014-05-15 19:00:00", 1),
+	(NULL, NULL, "2014-05-05 09:00:00", 2),
+	(NULL, NULL, "2014-05-10 09:00:00", 2),
+	(NULL, NULL, "2014-05-13 09:00:00", 2)
+;
